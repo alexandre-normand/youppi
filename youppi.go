69b9103988f4a17c0bb61a47177d28f6eb7f72ee
Alexandre Normand <alexandre.normand@opower.com>
2015-10-22 08:23:56 -0700

    Remove Unncessary Boilerplate Code Now in Slackscot.

--- youppi.go
+++ youppi.go
@@ -20,45 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var youppi Youppi
+	youppi := slackscot.NewSlackscot([]slackscot.ExtentionBundle{brain.NewKarma(), brain.NewImager()})
 
-	// Registers deferred call to close resources on shutdown
-	defer youppi.Close()
-
-	slackscot.Run(youppi, *config)
-}
-
-type Youppi struct {
-	bundles []slackscot.ExtentionBundle
-}
-
-func (youppi Youppi) Init(config config.Configuration) (commands []slackscot.Action, listeners []slackscot.Action, err error) {
-	karma := brain.NewKarma()
-	c, l, err := karma.Init(config)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	commands = append(commands, c...)
-	listeners = append(listeners, l...)
-
-	imager := brain.NewImager()
-	c, l, err = imager.Init(config)
-	if err != nil {
-		return nil, nil, err
-	}
-	commands = append(commands, c...)
-	listeners = append(listeners, l...)
-
-	//initImagesExt(config)
-	//initServiceCheck(config)
-	return commands, listeners, nil
-
-}
-
-func (youppi Youppi) Close() {
-	// Close any resources needed by scripts
-	for _, b := range youppi.bundles {
-		b.Close()
-	}
+	slackscot.Run(*youppi, *config)
 }
